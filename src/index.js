5c74d138374bd25f9282c2f8780c70c38cbd9cc6
unknown <radosttina>
2022-07-03 21:53:56 +0300

    Enable keyboard input

--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
+const NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const OPERATION_SYMBOLS = ["*", "+", "/", "-", "."];
+const RESULT_KEYS = ["=", "Enter"];
+const DELETE_KEY = "Backspace";
 
 const resultElement = document.getElementById("result");
 let calculation = "";
 
-const onEnterInput = (e) => {
-  const inputValue = e.target.textContent;
+const onEnterInput = (inputValue) => {
+  if (![...NUMBERS, ...OPERATION_SYMBOLS].includes(inputValue)) {
+    return;
+  }
 
   // don't allow the first symbol to be special symbol
   if (!calculation.length && isSpecialSymbol(inputValue)) {
     return;
   }
 
-  if (shouldOverwriteOperation(calculation, inputValue)) {
+  if (shouldOverwriteLastOperation(calculation, inputValue)) {
     calculation = calculation.replace(/[.+*/\-=]$/g, inputValue);
   } else {
     calculation += inputValue;
   }
 
   updateDisplay(calculation);
+};
+
+const onKeyboardInput = (e) => {
+  const inputValue = e.key;
+
+  if (RESULT_KEYS.includes(inputValue)) {
+    onCalculateResult();
+    return;
+  }
+
+  if (inputValue === DELETE_KEY) {
+    onDeleteLastCharacted();
+  }
+
+  onEnterInput(e.key);
+};
+
+const onVirtualCalculatorInput = (e) => {
+  onEnterInput(e.target.textContent);
 };
 
 const onCalculateResult = () => {
@@ -60,8 +84,10 @@ updateDisplay = (value) => {
 
 calculateStringEquation = (equation) => new Function("return " + equation)();
 
-shouldOverwriteOperation = (calculation, newOperation) =>
+shouldOverwriteLastOperation = (calculation, newOperation) =>
   isSpecialSymbol(calculation[calculation.length - 1]) &&
   isSpecialSymbol(newOperation);
 
 isSpecialSymbol = (value) => OPERATION_SYMBOLS.includes(value);
+
+document.addEventListener("keydown", onKeyboardInput);
